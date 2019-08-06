ca47c18cb142b6ae979cae9fbbcb2521f65ec533
Paolo Patierno <ppatierno@live.com>
2019-08-06 18:09:07 +0200

    Replaced MySQL with PostgreSQL

---
+++ kafka-connect-debezium/postgres/devices.sql
@@ -0,0 +1,13 @@
+-- Create the schema that we'll use to populate data and watch the effect in the binlog
+CREATE SCHEMA devices;
+SET search_path TO devices;
+
+-- Create and populate our products using a single insert with many rows
+CREATE TABLE deviceinfo (
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  manufacturer VARCHAR(255) NOT NULL
+);
+
+INSERT INTO deviceinfo
+VALUES ('1', 'manufacturer-A'),
+        ('2', 'manufacturer-B');
